0664d56f19caf89ecdf63bbc261886ea3c1104bf
Tumaini8 <lutufyotumaini6@gmail.com>
2021-11-06 14:26:34 +0300

    added a GITHUB Link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <Intro/>
+          <Intro />
+          <p className="info text-center mt-3">
+            Project created using react and it is an{" "}
+            <a href="https://github.com/Tumaini8/intro-component" rel="noreferrer" target="_blank">
+              open-source from GITHUB
+            </a>
+          </p>
         </header>
       </div>
     </div>
